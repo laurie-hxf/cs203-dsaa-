aec80fe078d819bb75fb6351e3d64ab98e1702ea
laurie-hxf <laurie@muggle-2.local>
2024-09-11 20:42:37 +0800

    add lab0d

---
+++ lab0/lab0d/src/Main.java
@@ -0,0 +1,108 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        //sample
+        QReader in = new QReader();
+        QWriter out = new QWriter();
+        int number_testcase=in.nextInt();
+        for(int i=0;i<number_testcase;i++){
+            int each_case_number= in.nextInt();
+            int[]arr=new int[each_case_number];
+            int min=100001;
+            int min_locate=0;
+            int max=-100001;
+            int max_locate=0;
+            for(int j=0;j<each_case_number;j++){
+                int input= in.nextInt();
+                if(input>max){
+                    max=input;
+                    max_locate=j;
+                }
+                if(input<min){
+                    min=input;
+                    min_locate=j;
+                }
+            }
+            if(max_locate<=min_locate){
+                out.println(max-min);
+            }
+        }
+        //out.println("Hello World");
+        out.close();
+    }
+}
+
+class QReader {
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer tokenizer = new StringTokenizer("");
+
+    private String innerNextLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public boolean hasNext() {
+        while (!tokenizer.hasMoreTokens()) {
+            String nextLine = innerNextLine();
+            if (nextLine == null) {
+                return false;
+            }
+            tokenizer = new StringTokenizer(nextLine);
+        }
+        return true;
+    }
+
+    public String nextLine() {
+        tokenizer = new StringTokenizer("");
+        return innerNextLine();
+    }
+
+    public String next() {
+        hasNext();
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
+class QWriter implements Closeable {
+    private BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public void print(Object object) {
+        try {
+            writer.write(object.toString());
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    public void println(Object object) {
+        try {
+            writer.write(object.toString());
+            writer.write("\n");
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            return;
+        }
+    }
+}
