80c636ecc33b3faaafbdbd20cd18dd523a53c8c6
laurie-hxf <1847238294@qq.com>
2024-11-29 11:18:06 +0800

    finish lab6f

--- lab6/lab6f/Main.cpp
+++ lab6/lab6f/Main.cpp
@@ -6,17 +6,21 @@
 using namespace std;
 struct Node {
   int index;
-  int p_i;
-  int e_i;
+  long long p_i;
+  long long e_i;
   int flag;
+  int father;
   vector<int> children;
-  Node() : index(0), p_i(0), flag(1), children(){}
+  Node() : index(0), p_i(0), flag(1),father(0){}
   Node(int index):index(index) {
     flag = 1;//true
     p_i=0;
     e_i=0;
+    father=0;
   }
 };
+void dfs(Node &node,vector<Node> &nodes) ;
+void dfs_2(Node &node,vector<Node> &nodes);
 int main() {
   int n;
   scanf("%d",&n);
@@ -24,7 +28,7 @@ int main() {
   for(int i=1;i<n+1;i++) {
     nodes[i]= Node(i);
   }
-  for(int i=1;i<n+1;i++) {
+  for(int i=1;i<n;i++) {
     int node1,node2;
     scanf("%d %d",&node1,&node2);
     nodes[node1].children.push_back(node2);
@@ -32,16 +36,105 @@ int main() {
   }
   int root=0;
   for(int i=1;i<n+1;i++) {
-    int p_i;
-    scanf("%d",&p_i);
+    long long p_i;
+    scanf("%lld",&p_i);
     nodes[i].p_i=p_i;
-    if(p_i>root) {
+    nodes[i].e_i=p_i;
+    if(p_i>nodes[root].p_i) {
       root = i;
     }
   }
-  int sub_tree=nodes[root].children.size();
-  for(int i=0;i<sub_tree;i++) {
-    
+  Node &root_node=nodes[root];
+  dfs(root_node,nodes);
+  int sub_tree=root_node.children.size();
+
+  if(sub_tree>1) {
+    long long largest=0;
+    long long largest_e_i=0;
+    long long second_largest=0;
+    long long second_largest_e_i=0;
+    for(int i=0;i<sub_tree;i++) {
+      Node child=nodes[root_node.children[i]];
+      if(child.e_i>largest_e_i) {
+        long long temp=largest;
+        long long temp_e_i=largest_e_i;
+        largest=child.index;
+        largest_e_i=child.e_i;
+        second_largest=temp;
+        second_largest_e_i=temp_e_i;
+      }else if(child.e_i>second_largest_e_i) {
+        second_largest=child.index;
+        second_largest_e_i=child.e_i;
+      }
+    }
+    nodes[largest].e_i=root_node.e_i;
+    nodes[second_largest].e_i=root_node.e_i;
+    root_node.flag=1;
+    for(int i=0;i<sub_tree;i++) {
+      dfs_2(nodes[root_node.children[i]],nodes);
+    }
   }
+  else if(sub_tree==1) {
+    Node &child=nodes[root_node.children[0]];
+    child.e_i=root_node.e_i;
+    root_node.flag=1;
+    dfs_2(child,nodes);
+  }
+  long long sum =0;
+  for(int i=0;i<n+1;i++) {
+    if(nodes[i].children.size()==1) {
+      sum+=nodes[i].e_i;
+    }
+  }
+  printf("%lld",sum);
   return 0;
 }
+
+
+void dfs(Node &node,vector<Node> &nodes) {
+  node.flag=0;
+  if(node.children.size()==1&&node.father!=0) {
+    node.e_i=node.p_i;
+    return ;
+  }
+  for(int i=0;i<node.children.size();i++) {
+    Node &temp=nodes[node.children[i]];
+    if(nodes[node.children[i]].flag==1) {
+      temp.father=node.index;
+      dfs(nodes[node.children[i]],nodes);
+    }
+  }
+  for(int i=0;i<node.children.size();i++) {
+    Node child=nodes[node.children[i]];
+    if(node.e_i<child.e_i&&child.index!=node.father) {
+      node.e_i=child.e_i;
+    }
+  }
+}
+
+void dfs_2(Node &node,vector<Node> &nodes) {
+  node.flag=1;
+  node;
+  int max=0;
+  for(int i=0;i<node.children.size();i++) {
+    if(nodes[node.children[i]].flag==0) {
+      Node &temp=nodes[node.children[i]];
+      if(temp.e_i>nodes[max].e_i) {
+        max=temp.index;
+      }
+    }
+  }
+  for(int i=0;i<node.children.size();i++) {
+    Node &temp=nodes[node.children[i]];
+    if(nodes[node.children[i]].flag==0) {
+      if(temp.index==max) {
+        nodes[max].e_i=node.e_i;
+        dfs_2(nodes[node.children[i]],nodes);
+      }else {
+        dfs_2(nodes[node.children[i]],nodes);
+      }
+    }
+  }
+}
+
+
